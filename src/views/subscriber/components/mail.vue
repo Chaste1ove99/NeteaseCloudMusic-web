6a5aa262aae8ce181afaa44b87623a7d70c4d852
Chastelove <373841822@qq.com>
2020-12-10 19:42:37 +0800

    优化防抖性能

--- src/views/subscriber/components/mail.vue
+++ src/views/subscriber/components/mail.vue
@@ -4,14 +4,15 @@
         <div class="text_wrap" id='wrap' ref='wrap'>
           <div v-if="more" @click="getmore" class="more_info">查看过去消息</div>
             <div v-for="(item,index) in newmsgs" :key="index" class="text_block" :id="index">
-                <div v-if="item.fromUser.nickname === profile.nickname" class="user_msg">
+                <div v-if="item.fromUser.userId === profile.userId" class="user_msg">
                         <el-image
                         class="user_icon"
       style="width: 40px; height: 40px"
       :src="item.fromUser.avatarUrl"
       fit="cover"></el-image>
-                    <div class="user_title">{{JSON.parse(item.msg).msg}}</div></div>
-                <div v-else class="my_msg">{{JSON.parse(item.msg).msg}}</div>
+                    <div class="user_title">{{JSON.parse(item.msg).msg}}</div>
+                </div>
+                <div v-else-if="item.toUser.userId === profile.userId" class="my_msg">{{JSON.parse(item.msg).msg}}</div>
             </div>
         </div>
         <div class="text_input">
@@ -36,8 +37,8 @@ export default {
       content: '',
       more: false,
       before: 0,
-      timeout: null,
-      loading: false
+      loading: false,
+      n: 0
     }
   },
   props: ['profile'],
@@ -51,23 +52,28 @@ export default {
     async function getoldmsg () {
       const result = await getPrivateHistory(_this.userid, 0, timestamp)
       const length = result.data.msgs.length
-      _this.before = result.data.msgs[length - 1].time
-      // reverse会改变原数组,所以这里reverse一次就可以 每次循环都只reverse一次
-      if (_this.newmsgs[0].time === result.data.msgs.reverse()[0].time) {
-        return false
+      if (length > 0) {
+        _this.before = result.data.msgs[length - 1].time
+        // reverse会改变原数组,所以这里reverse一次就可以 每次循环都只reverse一次
+        if (_this.newmsgs[0].time === result.data.msgs.reverse()[0].time) {
+          return false
+        } else {
+          // 第一次要清空原始数组
+          _this.newmsgs = []
+          _this.newmsgs = _this.newmsgs.concat(result.data.msgs)
+        }
+        _this.more = result.data.more
       } else {
-        // 第一次要清空原始数组
         _this.newmsgs = []
-        _this.newmsgs = _this.newmsgs.concat(result.data.msgs)
+        return false
       }
-      _this.more = result.data.more
     }
     getoldmsg()
     // 设置定时器 每三秒执行一次防抖函数 三秒发送一次请求
     window.setInterval(() => this.debounce(this.getMsg(0), 3000), 3000)
     this.$nextTick(() => {
       const wrap = this.$refs.wrap
-      console.log(wrap.scrollHeight)
+      // console.log(wrap.scrollHeight)
       // 这里获取到scrollHeight为390 其实是容器的高
       // 对比发现这里的v-for dom其实没渲染 缺少解决办法
       // 目前的方法是通过loading过渡 避免视觉上的瑕疵
@@ -75,12 +81,12 @@ export default {
         wrap.scrollBy(0, 999)
         _this.loading = false
       }
-      setTimeout(() => endloading(), 300)
+      setTimeout(() => endloading(), 1000)
     })
   },
   methods: {
     sendmsg (msg) {
-      this.timeout = null
+      this.debounce(this.getMsg(0), 3000)
       const timestamp = Date.parse(new Date())
       const wrap = document.getElementById('wrap')
       const _this = this
@@ -88,7 +94,13 @@ export default {
         const result = await sendtext(_this.userid, timestamp, msg)
         // console.log(result)
         // 视觉上存在一点BUG 发送消息后默认数据被隐藏
-        _this.newmsgs.push(result.data.newMsgs[0])
+        const lengthB = _this.newmsgs.length
+        // 再次防抖
+        if (_this.newmsgs[lengthB - 1].time === result.data.newMsgs[0].time) {
+          return false
+        } else {
+          _this.newmsgs.push(result.data.newMsgs[0])
+        }
         // 由于vue异步更新dom 等待dom更新后再下拉到底
         _this.$nextTick(() => {
           wrap.scrollBy(0, 9999)
@@ -104,7 +116,6 @@ export default {
     },
     // 获取私信内容
     getMsg (e) {
-      this.timeout = null
       const timestamp = Date.parse(new Date())
       const _this = this
       const wrap = document.getElementById('wrap')
@@ -112,32 +123,37 @@ export default {
         const result = await getPrivateHistory(_this.userid, e, timestamp)
         const lengthA = result.data.msgs.length
         const lengthB = _this.newmsgs.length
+        // 如果聊天记录为空
+        if (result.data.msgs.length > 0) {
         // reverse会改变原数组,所以这里reverse一次就可以 每次循环都只reverse一次
-        if (_this.newmsgs[lengthB - 1].time === result.data.msgs.reverse()[lengthA - 1].time) {
-          return false
-        } else {
-          const index = result.data.msgs.findIndex(v => v.time === _this.newmsgs[lengthB - 1].time)
-          // console.log(index)
-          if (index === -1) {
-            _this.newmsgs = _this.newmsgs.concat(result.data.msgs)
-            // slice方法限制 分两种情况
-          } else if (index === lengthA - 2) {
-            // const message = result.data.msgs.slice(index + 1, lengthA).push(result.data.msgs[lengthA])
-            // console.log(message)
-            _this.newmsgs.push(result.data.msgs[lengthA - 1])
-            _this.$nextTick(() => {
-              wrap.scrollBy(0, 9999)
-            })
-            // console.log(_this.newmsgs)
+          if (_this.newmsgs[lengthB - 1].time === result.data.msgs.reverse()[lengthA - 1].time) {
+            return false
           } else {
-            const message = result.data.msgs.slice(index + 1, lengthA - 1)
-            message.push(result.data.msgs[lengthA - 1])
-            _this.newmsgs = _this.newmsgs.concat(message)
-            _this.$nextTick(() => {
-              wrap.scrollBy(0, 9999)
-            })
+            const index = result.data.msgs.findIndex(v => v.time === _this.newmsgs[lengthB - 1].time)
+            // console.log(index)
+            if (index === -1) {
+              _this.newmsgs = _this.newmsgs.concat(result.data.msgs)
+              // slice方法限制 分两种情况
+            } else if (index === lengthA - 2) {
+              // const message = result.data.msgs.slice(index + 1, lengthA).push(result.data.msgs[lengthA])
+              // console.log(message)
+              _this.newmsgs.push(result.data.msgs[lengthA - 1])
+              _this.$nextTick(() => {
+                wrap.scrollBy(0, 9999)
+              })
+              // console.log(_this.newmsgs)
+            } else {
+              const message = result.data.msgs.slice(index + 1, lengthA - 1)
+              message.push(result.data.msgs[lengthA - 1])
+              _this.newmsgs = _this.newmsgs.concat(message)
+              _this.$nextTick(() => {
+                wrap.scrollBy(0, 9999)
+              })
+            }
           }
           // _this.newmsgs = _this.newmsgs.concat(result.data.msgs)
+        } else if (result.data.msgs.length === 0) {
+          return false
         }
       }
       getoldmsg()
@@ -151,17 +167,18 @@ export default {
     closemenu () {
       this.$emit('foldmenu', 0)
     },
-    // 防抖 本质是个闭包结构
+    // 防抖
     debounce (fn, wait) {
+      var timeout = null
       return function () {
         if (this.timeout !== null) {
-          clearTimeout(this.timeout)
+          clearTimeout(timeout)
         }
-        this.timeout = setTimeout(fn, wait)
+        timeout = setTimeout(fn, wait)
       }
     },
     getmore () {
-      this.timeout = null
+      this.debounce(this.getMsg(0), 3000)
       const timestamp = Date.parse(new Date())
       const _this = this
       async function getoldmsg () {
@@ -181,7 +198,6 @@ export default {
     }
   },
   destroyed () {
-    clearTimeout(this.timeout)
   }
 }
 </script>
@@ -215,29 +231,29 @@ export default {
       }
       .text_block{
           margin-bottom: 10px;
-          position: relative;
-          font-size: 14px;
           overflow: hidden;
+          font-size: 14px;
+          margin-top: 10px;
           .user_msg{
-            left: 0;
+            float: left;
             position: relative;
             .user_icon{
             border-radius: 20px;
+            position: absolute;
+            top: 50%;
+            transform: translateY(-50%);
             }
             .user_title{
-                position: absolute;
-                top: 50%;
-                transform: translateY(-50%);
                 background-color: #ccc;
                 padding: 5px;
                 border-radius: 8px;
-                display: inline-block;
-                margin-left: 10px;
+                margin-left: 50px;
                 max-width: 200px;
             }
           }
           .my_msg{
-              float: right;
+            float: right;
+            display: block;
             right: 0;
             color: #fff;
             background-color: red;
